9691bce469116a8adeefd513b6c389ca4aec6c4f
Brianna Dirks <148362578+bridirks@users.noreply.github.com>
2024-12-04 16:49:53 -0500

    Implemented test for HazardApplication #18 (#51)

--- src/test/java/gov/epa/ccte/api/hazard/HazardApplicationTests.java
+++ src/test/java/gov/epa/ccte/api/hazard/HazardApplicationTests.java
@@ -1,15 +1,31 @@
 package gov.epa.ccte.api.hazard;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 @SpringBootTest
+@Testcontainers
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class HazardApplicationTests {
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> pgsqldb = new PostgreSQLContainer<>("postgres:13-alpine");
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void applicationStarts(){
+		assertThatCode(() -> HazardApplication.main(new String[]{})).doesNotThrowAnyException();
+	}
 }
