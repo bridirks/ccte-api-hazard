60282a11c8990327f7d078398573f542794973ba
Brianna Dirks <148362578+bridirks@users.noreply.github.com>
2025-01-13 10:14:45 -0500

    Delete src/main/java/gov/epa/ccte/api/hazard/projection/toxrefdata/ToxRefDataProjection.java

--- src/main/java/gov/epa/ccte/api/hazard/projection/toxrefdata/ToxRefDataProjection.java
+++
@@ -1,8 +0,0 @@
-package gov.epa.ccte.api.hazard.projection.toxrefdata;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@Schema(enumAsRef = true,name = "ToxRefDataProjection", description = "Projection options for ToxRefDB Data APIs ")
-public enum ToxRefDataProjection {
-    ToxRefDataSummary, ToxRefDataAll
-}
