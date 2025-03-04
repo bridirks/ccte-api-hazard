0e47e4632517ce0e7655f6eb68b743be834a98be
Brianna Dirks <148362578+bridirks@users.noreply.github.com>
2025-03-04 09:27:53 -0500

    Update API documentation with domain classes description for (#67)
    CancerSummary #35

--- src/main/java/gov/epa/ccte/api/hazard/domain/CancerSummary.java
+++ src/main/java/gov/epa/ccte/api/hazard/domain/CancerSummary.java
@@ -2,7 +2,7 @@ package gov.epa.ccte.api.hazard.domain;
 
 import lombok.Getter;
 import lombok.Setter;
-
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -13,6 +13,7 @@ import jakarta.validation.constraints.Size;
 @Entity
 @Getter
 @Setter
+@Schema(description = "A carcinogen is a substance that may cause cancer and pose a health hazard. Carcinogens are subject to specific controls and regulations. National and international health agencies evaluate new and existing chemicals to determine if they are likely to be carcinogens through a process called cancer hazard identification. This process combines human and animal data with supporting evidence to characterize the weight-of-evidence (WOE) regarding the agent's potential as a human carcinogen. The general categories recognized by the guidelines are carcinogenic to humans, likely to be carcinogenic to humans, and suggestive evidence of carcinogenic potential. This endpoint provides summary-level cancer data associated to a chemical.")
 @Table(name = "cancer_summary", schema = "ms")
 public class CancerSummary {
     @Id
@@ -22,21 +23,26 @@ public class CancerSummary {
 
     @Size(max = 255)
     @Column(name = "cancer_call")
+    @Schema(description = "Annotation indicating level of carcinogenicity")
     private String cancerCall;
 
     @Size(max = 255)
+    @Schema(description = "Distributed Structure-Searchable Toxicity (DSSTox) id")
     private String dtxsid;
 
     @Size(max = 255)
     @Column(name = "exposure_route")
+    @Schema(description = "Path by which test substance was administered to animal. Options include oral, dermal, inhalation, injection, or other")
     private String exposureRoute;
 
     private Long rn;
 
     @Size(max = 255)
+    @Schema(description = "Originator of source data")
     private String source;
 
     @Size(max = 255)
+    @Schema(description = "URL for source data")
     private String url;
 
 }
