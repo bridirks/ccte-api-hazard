10460a3ec4147673c96113636a236cc97ad3d6e1
Brianna Dirks <148362578+bridirks@users.noreply.github.com>
2025-03-04 09:28:50 -0500

    Update API documentation with domain classes description for (#68)
    GenetoxSummary #36

--- src/main/java/gov/epa/ccte/api/hazard/domain/GenetoxSummary.java
+++ src/main/java/gov/epa/ccte/api/hazard/domain/GenetoxSummary.java
@@ -1,5 +1,6 @@
 package gov.epa.ccte.api.hazard.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -11,6 +12,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Schema(description = "Description: Genotoxicity testing, also known as short-term assays, is used to evaluate the potential adverse effects of chemicals on human health. These tests can help identify carcinogens, as about 80% of known human carcinogens are genotoxic. The results of genotoxicity tests are important because of the irreversible nature and severity of the health effects that may result from genotoxic events. There are many different techniques that can be used for genotoxicity testing, notably including Ames test (comparative genetic analysis using multiple bacterial strains of Salmonella typhimurium), Comet assay (measurement DNA strand breaks), micronucleus test (cytogenetic assay that uses fluorescence in situ hybridization and chromosome painting) and chromosomal aberration test (cytogenetic assay that examines in vivo tissues to reflect how chemicals are absorbed, excreted, distributed, and metabolized). This endpoint provides summary-level genotoxicity data associated to a chemical.")
 @Entity
 @Table(name = "genetox_summary", schema = "ms")
 public class GenetoxSummary {
@@ -21,23 +23,29 @@ public class GenetoxSummary {
 
     @Size(max = 255)
     @Column(name = "dtxsid")
+    @Schema(description = "Distributed Structure-Searchable Toxicity (DSSTox) id")
     private String dtxsid;
 
     @Column(name = "reports_pos")
+    @Schema(description = "Count of genetox records reporting positive results")
     private Integer reportsPositive;
 
     @Column(name = "reports_neg")
+    @Schema(description = "Count of genetox records reporting negative results")
     private Integer reportsNegative;
 
     @Column(name = "reports_other")
+    @Schema(description = "Count of genetox records reporting ‘other’ in results")
     private Integer reportsOther;
 
     @Size(max = 255)
     @Column(name = "ames")
+    @Schema(description = "Average result in AMES test")
     private String ames;
 
     @Size(max = 255)
     @Column(name = "micronucleus")
+    @Schema(description = "Average result in Micronucleus test")
     private String micronucleus;
 
     @Column(name = "rn")
