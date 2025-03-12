f93f749301690ef7ef0be6f7405296c56d7095ce
Brianna Dirks <148362578+bridirks@users.noreply.github.com>
2025-03-12 12:03:53 -0400

    Update GenetoxDetailRepositoryTest.java

--- src/test/java/gov/epa/ccte/api/hazard/datatest/GenetoxDetailRepositoryTest.java
+++ src/test/java/gov/epa/ccte/api/hazard/datatest/GenetoxDetailRepositoryTest.java
@@ -1,6 +1,5 @@
 package gov.epa.ccte.api.hazard.datatest;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -29,12 +28,6 @@ class GenetoxDetailRepositoryTest {
     @Autowired private JdbcTemplate jdbcTemplate;
     @Autowired private TestEntityManager entityManager;
     @Autowired private GenetoxDetailRepository repository;
-
-    @AfterEach
-    void setup() {
-    	repository.deleteAll(); // Clean up after each test
-    }
-    
 
     @Test
     void injectedComponentsAreNotNull() {
