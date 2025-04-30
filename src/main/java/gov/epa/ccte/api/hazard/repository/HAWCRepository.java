5f138dc8e8627cae39f543154f5692bb35a48822
Brianna Dirks <148362578+bridirks@users.noreply.github.com>
2025-04-30 11:05:16 -0400

    Update HAWCRepository.java

--- src/main/java/gov/epa/ccte/api/hazard/repository/HAWCRepository.java
+++ src/main/java/gov/epa/ccte/api/hazard/repository/HAWCRepository.java
@@ -6,11 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
 
-import gov.epa.ccte.api.hazard.domain.HAWC;
+import gov.epa.ccte.api.hazard.domain.Hawc;
 
 @SuppressWarnings("unused")
 @RepositoryRestResource(exported = false)
-public interface HAWCRepository extends JpaRepository<HAWC, Integer>{
+public interface HAWCRepository extends JpaRepository<Hawc, Integer>{
 
     @Transactional(readOnly = true)
     <T>List<T> findByDtxsid(String dtxsid);
